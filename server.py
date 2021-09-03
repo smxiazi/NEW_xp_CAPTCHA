bec3805d0575b312b631ea5e875e50391db3e9b4
算命縖子 <30351807+smxiazi@users.noreply.github.com>
2021-09-03 21:54:56 +0800

    Update server.py

--- server.py
+++ server.py
@@ -28,44 +28,53 @@ class Resquest(BaseHTTPRequestHandler):
     def do_POST(self):
         #print(self.headers)
         #print(self.command)
-        if self.path != '/base64':
-            self.send_error(404, "Page not Found!")
-            return
+        text = ''
+        try:
+            if self.path != '/base64':
+                self.send_error(404, "Page not Found!")
+                return
 
-        img_name = time.time()
-        req_datas = self.rfile.read(int(self.headers['content-length']))
-        req_datas = req_datas.decode()
-        base64_img = re.search('base64=(.*?)$',req_datas)
-        #print(base64_img.group(1)) #post base64参数的内容
+            img_name = time.time()
+            req_datas = self.rfile.read(int(self.headers['content-length']))
+            req_datas = req_datas.decode()
+            base64_img = re.search('base64=(.*?)$',req_datas)
+            #print(base64_img.group(1)) #post base64参数的内容
 
-        with open("temp/%s.png"%img_name, 'wb') as f:
-            f.write(base64.b64decode(base64_img.group(1)))
-            f.close()
+            with open("temp/%s.png"%img_name, 'wb') as f:
+                f.write(base64.b64decode(base64_img.group(1)))
+                f.close()
 
-        #验证码识别
-        sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
-        with open(r"temp/%s.png"%img_name, "rb") as f:
-            b = f.read()
-        text = sdk.predict(image_bytes=b)
-        print(text) #识别的结果
+            #验证码识别
+            sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
+            with open(r"temp/%s.png"%img_name, "rb") as f:
+                b = f.read()
+            text = sdk.predict(image_bytes=b)
+            print('\n'+text+'\n') #识别的结果
 
-        #保存最新count个的验证码及识别结果
-        with open('temp/log.txt', 'r') as f:
-            data = ""
-            counts = 0
-            content = f.read()
-            pattern = re.compile(r'.*?\n')
-            result1 = pattern.findall(content)
-            for i in result1:
-                counts += 1
-                if counts >= count: break
-                data = data + i
+            #保存最新count个的验证码及识别结果
+            with open('temp/log.txt', 'r') as f:
+                data = ""
+                counts = 0
+                content = f.read()
+                pattern = re.compile(r'.*?\n')
+                result1 = pattern.findall(content)
+                for i in result1:
+                    counts += 1
+                    if counts >= count: break
+                    data = data + i
 
-        with open('temp/log.txt', 'w') as f:
-            f.write('<tr align=center><td><img src="data:image/png;base64,%s"/></td><td>%s</td><td>%s</td></tr>\n'%(base64_img.group(1),text,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(img_name))))+ data)
+            with open('temp/log.txt', 'w') as f:
+                f.write('<tr align=center><td><img src="data:image/png;base64,%s"/></td><td>%s</td><td>%s</td></tr>\n'%(base64_img.group(1),text,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(img_name))))+ data)
 
-        #删除掉图片文件，以防占用太大的内存
-        os.remove("temp/%s.png"%img_name)
+            #删除掉图片文件，以防占用太大的内存
+            os.remove("temp/%s.png"%img_name)
+        except:
+            text= '0000'
+            print('\n识别失败！\n')
+        
+        if text =='':
+            text= '0000'
+            print('\n识别失败！\n')
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
